1b11df811d4efd1b3406b8ef10905455e6c8b572
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2015-06-04 12:31:39 +0800

    Added initial test

--- test/gjs/core_test.clj
+++ test/gjs/core_test.clj
@@ -2,6 +2,22 @@
   (:require [clojure.test :refer :all]
             [gjs.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def auction  (FakeAuctionServer. "item-54321"))
+(def application (ApplicationRunner.))
+
+(defn stop-auction [f]
+  (f)
+  (stop. auction))
+
+(defn stop-application [f]
+  (f)
+  (stop. application))
+
+(use-fixtures :each stop-auction stop-application)
+
+(deftest sniper-joins-auction-until-auction-closes
+  (startSellingItem. auction)
+  (startBiddingIn. application auction)
+  (hasReceivedJoinRequestFromSniper. auction)
+  (announceClosed. auction)
+  (showsSniperHasLostAuction. application))
